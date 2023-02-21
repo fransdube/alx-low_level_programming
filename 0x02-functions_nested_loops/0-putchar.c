e70fcc536f1306148b7a52d50832ccbbe162287c
fransdube <fransdube@gmail.com>
2023-02-21 04:16:26 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by frans
+ * main - Entry File  Block
+  *Description: prints _putchar followed by a new line
  * Return: 0
  */
-
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[8] = "_putchar";
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
-
