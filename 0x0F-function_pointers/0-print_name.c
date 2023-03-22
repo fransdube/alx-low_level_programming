e9952109209530e515dfad63f4f604c863c96a34
FM Dube <31796906+fransdube@users.noreply.github.com>
2023-03-22 13:05:07 +0200

    Projects for 0x0F. C - Function pointers
    Learning about pointer functions and function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function to call a function
+ *
+ * @name: Parameter 1
+ * @f: parameter function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
+}
