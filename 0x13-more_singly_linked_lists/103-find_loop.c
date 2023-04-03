76cd6e527e5e73fe0d9ee52c1ea9aa9f6ced7a66
FM Dube <31796906+fransdube@users.noreply.github.com>
2023-04-03 15:50:04 +0200

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,5 @@
 #include "lists.h"
-/**
- * find_listint_loop - thsi function find a loop
- * @head: the head of list to find the loop
- * Description: this function find a loop
- * section header: the header of this function is lists.h)*
- * Return: the node were the loop is located
- */
+
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *idontknow, *idontknowagain;
