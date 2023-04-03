33958e30e330606fd8e38843684b080bbc573f61
FM Dube <31796906+fransdube@users.noreply.github.com>
2023-04-03 15:49:25 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,12 +1,5 @@
 #include "lists.h"
-/**
- * free_listint - free a list
-(* a blank line
-*@head: the head of list
-* Description: this function free a list.)?
-(* section header: the header of this function is lists.h)*
-* Return: void, no return
-*/
+
 void free_listint(listint_t *head)
 {
 	listint_t *position;
