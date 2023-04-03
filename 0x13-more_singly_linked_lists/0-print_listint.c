3c0196aed2bc3be7383a17583d1551e18391ce02
FM Dube <31796906+fransdube@users.noreply.github.com>
2023-04-03 22:46:09 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+#include <stdio.h>
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
 
-	return (num);
+	return (nodes);
 }
